9a5ea55db490af2b573c4311af4d76d4a1795cdf
Michael Legg <michael.legg@outlook.com>
2020-11-25 23:00:38 +0000

    Updated socket-io port

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import router from "./router";
 import { io } from "socket.io-client";
 import VueSocketIOExt from "vue-socket.io-extended";
 
-export const socket = io(process.env.VUE_APP_SOCKET_IO_URL || "localhost:443", {
-  transports: ["websocket"],
-  autoConnect: false
-});
+export const socket = io(
+  process.env.VUE_APP_SOCKET_IO_URL || "localhost:3001",
+  {
+    transports: ["websocket"]
+  }
+);
 
 Vue.use(VueSocketIOExt, socket);
 
