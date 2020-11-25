341a2e53aa05861b51d8754a034e302acba46847
Michael Legg <michael.legg@outlook.com>
2020-11-25 20:02:25 +0000

    - Added router - Fixed browserlist - Removed setup code

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import Vue from "vue";
-
 import App from "./App.vue";
+import router from "./router";
+import { io } from "socket.io-client";
+import VueSocketIOExt from "vue-socket.io-extended";
+
+export const socket = io(process.env.VUE_APP_SOCKET_IO_URL || "localhost:443", {
+  transports: ["websocket"],
+  autoConnect: false
+});
+
+Vue.use(VueSocketIOExt, socket);
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="home">
+    <div class="content">
+      <h1>StoryPoint Ninja</h1>
+      <input placeholder="Enter session id" />
+      <br />
+      <button>Create new session</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss">
+.content {
+  text-align: center;
+}
+</style>
